301e7e3d4ad0c89c735b8e7ead28ce5a4aff45c1
nick-bennett <nbennett10@cnm.edu>
2024-01-22 11:55:51 -0700

    Simplify increment operation.

--- src/main/java/edu/cnm/deepdive/CommandLine.java
+++ src/main/java/edu/cnm/deepdive/CommandLine.java
@@ -4,7 +4,7 @@ public class CommandLine {
 
   public static void main(String[] args) {
     System.out.println(args.length);
-    for (int i = 0; i < args.length; i = i + 1) {
+    for (int i = 0; i < args.length; i++) {
       System.out.println(args[i]);
     }
   }
